f68657f172e4df954ab55463eeb4c450792d64c6
Sophie <sophie.thi.nguyen11@gmail.com>
2026-02-22 15:39:52 -0800

    enemy gradually damages player

--- scenes/player.gd
+++ scenes/player.gd
@@ -5,10 +5,11 @@ extends CharacterBody2D
 const SPEED := 600.0
 const ACCEL := 1.3
 
-var health := 1
+var health := 20
 
 var input: Vector2
-
+func _ready() -> void:
+	pass
 func getNormalizedInput():
 
 	input.x = Input.get_axis("left", "right")
@@ -20,7 +21,12 @@ func lookAtMouse(delta):
 	var new_transform := transform.looking_at(get_global_mouse_position())
 
 	transform = transform.interpolate_with(new_transform, 0.1)
-	
+
+func _process(delta: float) -> void:
+	if health == 0:
+		print("player die")
+		get_tree().call_deferred("reload_current_scene")
+
 func _physics_process(delta: float) -> void:	
 	# Get the input direction and handle the movement/deceleration.
 	getNormalizedInput()
@@ -31,6 +37,8 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
-# hit by enemy oh no dying
-func _on_enemy_player_hit() -> void:
-	get_tree().reload_current_scene()
+func lowerHeath(healthTaken):
+	health -= healthTaken
+	print("health is currently ", health)
+	
+	
--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,10 +1,15 @@
 class_name Enemy
 extends CharacterBody2D
 
+@onready var damage_timer: Timer = $DamageTimer
+
 @export var player: Player
 const MOVE_SPEED := 100.0
 signal playerHit
 
+var is_damaging := false
+
+var exitingPlayer := false
 func _ready() -> void:
 	if not player:
 		printerr("no player reference to enemy")
@@ -18,4 +23,21 @@ func _physics_process(delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
-		playerHit.emit()
+		is_damaging = true
+		damage_timer.start()
+
+# updates exiting player var
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body is Player:
+		is_damaging = false
+
+# damages player until exiting player
+func _on_damage_timer_timeout() -> void:
+	if is_damaging:
+		player.lowerHeath(1)
+		damage_timer.start()
+		
+		
+		
+
+	
