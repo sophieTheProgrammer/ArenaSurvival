defffa63bd8bead65fcba201a0e6082951a6876a
Sophie <sophie.thi.nguyen11@gmail.com>
2026-02-24 17:43:32 -0800

    added timer

--- scenes/health_bar.gd
+++ scenes/health_bar.gd
@@ -1,7 +1,1 @@
 extends ProgressBar
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-@onready var player = Global.player_node
-
-func _process(delta: float) -> void:
-	value = 100 * (Global.player_node.health / Global.player_node.STARTING_HEALTH)
---
+++ scripts/ui.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@onready var timer_number: RichTextLabel = $CanvasLayer/Panel/TimerNumber
+@onready var health_bar: ProgressBar = $CanvasLayer/HealthBar
+@onready var player = Global.player_node
+
+var time : float =  0.0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	time += delta
+	health_bar.value = 100 * (Global.player_node.health / Global.player_node.STARTING_HEALTH)
+	
+	timer_number.text = str(snapped(time, 0.1))
