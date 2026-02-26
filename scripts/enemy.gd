878e64d00cbb544163961ee47cd00b747a05f94c
Sophie <sophie.thi.nguyen11@gmail.com>
2026-02-26 15:09:07 -0800

    player can go through bullet but bullet deletes enemy
    using different collision layesr

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -23,6 +23,7 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("entered")
 	if body is Player:
 		is_damaging = true
 		damage_timer.start()
